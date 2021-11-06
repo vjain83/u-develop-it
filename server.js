20e697170334308126a510365ba9eac1a6d44a50
Varsha Jain <csvarshajain@gmail.com>
2021-11-06 00:42:02 -0400

    updated api get with sql join query method

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ const db = mysql.createConnection({
 
 // Select candiate by id
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates. * , parties.name AS party_name FROM 
+    candidates LEFT JOIN parties ON candidates.party_id = parties.id  WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.query(sql, params, (err, row) => {
         if (err) {
@@ -83,19 +84,9 @@ app.post('/api/candidate', ({ body }, res) => {
 
 });
 
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//     VALUES(?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates. * , parties.name AS party_name FROM 
+    candidates LEFT JOIN parties ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
