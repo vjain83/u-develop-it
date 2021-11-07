0a84c692a788491cf2b74900772200e3c5c7f64e
Varsha Jain <csvarshajain@gmail.com>
2021-11-07 00:47:45 -0400

    voters table has been created

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
+DROP TABLE IF EXISTS voters;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
-
 
 
 CREATE TABLE parties (
@@ -17,4 +17,11 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 
+);
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
